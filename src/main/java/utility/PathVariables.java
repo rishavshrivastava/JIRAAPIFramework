ee65329386140327db4c4c6f3edc5f6553bead7c
Rishav Shrivastava <rishav.beee@gmail.com>
2020-06-15 08:23:54 +0530

    modified files

--- src/main/java/utility/PathVariables.java
+++ src/main/java/utility/PathVariables.java
@@ -2,7 +2,7 @@ package utility;
 
 public interface PathVariables {
 
-	static final String BaseURI = "http://localhost:8080";
+	static final String BaseURI = "http://localhost:9090";
 	static final String excelPath = "data//APIInputs.xlsx";
 	static final String sheetname = "Sheet1";
 }
